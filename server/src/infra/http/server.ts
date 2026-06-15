89fcfec17271d4e6833e05473a134dcfe3c395af
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-06-15 15:27:45 -0400

    Adicione validação de schemas e tratamento de erros com Zod no servidor

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,7 +1,25 @@
 import { fastify } from 'fastify'
 import { fastifyCors} from "@fastify/cors";
+import {env} from "@/env";
+import { serializerCompiler, validatorCompiler, hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation
+    })
+  }
+
+  console.error('Error handling request:', error)
+  reply.status(500).send({ message: 'Internal Server Error' })
+})
 
 server.register(fastifyCors, {
   origin: '*',
