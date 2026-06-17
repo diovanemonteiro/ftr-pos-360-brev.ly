6d66d22990d2f7026e4a515e579c4d0af2032e8d
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-06-17 14:17:13 -0400

    Altera validação na função `getLinks` para utilizar `zod` em vez de tipos manuais

--- server/src/app/functions/get-links.ts
+++ server/src/app/functions/get-links.ts
@@ -2,11 +2,14 @@ import { count } from 'drizzle-orm'
 import { db } from '@/infra/db'
 import * as schema from '@/infra/db/schemas'
 import { type Either, right } from '@/infra/shared/either'
+import { z } from 'zod'
 
-interface GetLinksInput {
-  page?: number
-  pageSize?: number
-}
+const getLinksInput = z.object({
+  page: z.number().optional().default(1),
+  pageSize: z.number().optional().default(20),
+})
+
+type GetLinksInput = z.input<typeof getLinksInput>
 
 interface GetLinksOutput {
   links: {
@@ -22,7 +25,8 @@ interface GetLinksOutput {
 export async function getLinks(
   input: GetLinksInput = {}
 ): Promise<Either<never, GetLinksOutput>> {
-  const { page = 1, pageSize = 20 } = input
+
+  const { page = 1, pageSize = 20 } = getLinksInput.parse(input)
   const offset = (page - 1) * pageSize
 
   const [links, [{ total }]] = await Promise.all([
