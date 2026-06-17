4dbddb0a0feae9796dcd5dbb58bb29d2460a3727
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-06-17 11:12:27 -0400

    Altera validação e retorno na rota `createLinkRoute` para utilizar `zod` e melhoria no tratamento de erros

--- server/src/infra/http/routes/create-link.ts
+++ server/src/infra/http/routes/create-link.ts
@@ -1,30 +1,40 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
+import {createLink} from "@/app/functions/create-link";
 
 export const createLinkRoute: FastifyPluginAsyncZod = async (server) => {
   server.post("/links", {
     schema: {
-      summary: "Create a new link",
+      summary: "Create a short url",
       tags: ["links"],
-      body: {
-        type: "object",
-        properties: {
-          url: {
-            type: "string",
-            format: "uri"
-          },
-          description: {
-            type: "string"
-          },
-        },
-        required: ["url"],
-      },
+      body: z.object({
+        originalUrl: z.string().url(),
+        shortUrl: z.string().min(3),
+      }),
       response: {
-        201: {
-          description: "Link created successfully",
-        },
+        201: z.object({
+          id: z.string(),
+          originalUrl: z.string(),
+          shortUrl: z.string(),
+          accessCount: z.number(),
+          createdAt: z.date(),
+        }),
+        409: z.object({ message: z.string() }),
+        422: z.object({ message: z.string() }),
       },
     },
   }, async (request, reply) => {
-    return reply.status(201).send();
+
+    const result = await createLink(request.body)
+
+    if (!result.success) {
+      if (result.error.name === 'ShortUrlAlreadyExists') {
+        return reply.status(409).send({ message: result.error.message })
+      }
+      return reply.status(422).send({ message: result.error.message })
+    }
+
+    return reply.status(201).send(result.value)
+
   });
 };
