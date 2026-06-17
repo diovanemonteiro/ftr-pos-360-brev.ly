cb6b49cf1d0bbcde2daa5265cb3f288bebf74823
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-06-17 10:09:35 -0400

    Corrige nomenclaturas e ajustes menores no servidor HTTP

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,6 +1,5 @@
 import { fastify } from 'fastify'
 import { fastifyCors} from "@fastify/cors";
-import {env} from "@/env";
 import {
   serializerCompiler,
   validatorCompiler,
@@ -9,7 +8,7 @@ import {
 } from 'fastify-type-provider-zod'
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
-import { createLink } from "@/infra/http/routes/create-link";
+import { createLinkRoute } from "@/infra/http/routes/create-link";
 
 const server = fastify()
 
@@ -29,14 +28,12 @@ server.setErrorHandler((error, request, reply) => {
   reply.status(500).send({ message: 'Internal Server Error' })
 })
 
-server.register(fastifyCors, {
-  origin: '*',
-})
+server.register(fastifyCors, { origin: '*' })
 
 server.register(fastifySwagger, {
   openapi: {
     info: {
-      title: 'Link Shortener API',
+      title: 'URL Shortener API',
       version: '1.0.0',
     },
   },
@@ -47,7 +44,7 @@ server.register(fastifySwaggerUi, {
   routePrefix: '/docs',
 })
 
-server.register(createLink)
+server.register(createLinkRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0'}).then(() => {
   console.log('HTTP server running on port 3333')
