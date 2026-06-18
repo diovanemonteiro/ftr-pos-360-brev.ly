680cfd20984255e19c3a4caf28c6439586786ca2
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-06-18 13:46:50 -0400

    Substitui dados estáticos por hook `useLinks` na página inicial

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -2,20 +2,24 @@ import { NewLink } from "@/components/new-link.tsx"
 import { LinkList } from "@/components/link-list.tsx"
 import { LinkListItem } from "@/components/link-list-item.tsx"
 import brevlyLogo from "@/assets/logo.svg"
+import {useLinks} from "@/hooks/use-links.ts";
 
 export function HomePage() {
 
-    const data = {
-        total: 5,
-        links: [
-            { id: 1, originalUrl: "https://www.google.com.br", shortUrl: "google", accessCount: 142 },
-            { id: 2, originalUrl: "https://www.github.com/diovane", shortUrl: "meu-github", accessCount: 87 },
-            { id: 3, originalUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", shortUrl: "yt-video", accessCount: 310 },
-            { id: 4, originalUrl: "https://tailwindcss.com/docs/installation", shortUrl: "tw-docs", accessCount: 23 },
-            { id: 5, originalUrl: "https://react.dev/learn", shortUrl: "react-learn", accessCount: 56 },
-        ],
-    }
-    const isLoading = false
+    const { data, isLoading } = useLinks()
+
+    // const data = {
+    //     total: 5,
+    //     links: [
+    //         { id: 1, originalUrl: "https://www.google.com.br", shortUrl: "google", accessCount: 142 },
+    //         { id: 2, originalUrl: "https://www.github.com/diovane", shortUrl: "meu-github", accessCount: 87 },
+    //         { id: 3, originalUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", shortUrl: "yt-video", accessCount: 310 },
+    //         { id: 4, originalUrl: "https://tailwindcss.com/docs/installation", shortUrl: "tw-docs", accessCount: 23 },
+    //         { id: 5, originalUrl: "https://react.dev/learn", shortUrl: "react-learn", accessCount: 56 },
+    //     ],
+    // }
+
+    // const isLoading = false
     const isExporting = false
 
     function handleSubmit(data: { originalUrl: string; shortUrl: string }) {
