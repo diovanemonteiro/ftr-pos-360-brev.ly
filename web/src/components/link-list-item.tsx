864b578f9cd2ce8f78242e1cd0161279bad8c198
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-06-20 18:05:44 -0400

    Adiciona componente `Spinner` e ajustes de classes em botões de `LinkListItem`

--- web/src/components/link-list-item.tsx
+++ web/src/components/link-list-item.tsx
@@ -9,6 +9,16 @@ type LinkListItemProps = {
     link: Link
 }
 
+export function Spinner() {
+    return (
+        <svg className="size-8 animate-spin text-blue-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path className="opacity-75" fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+    )
+}
+
 export function LinkListItem({ link } : LinkListItemProps) {
     const [copied, setCopied] = useState(false)
 
@@ -53,25 +63,21 @@ export function LinkListItem({ link } : LinkListItemProps) {
 
                 <Button
                     variant="secondary"
-                    className="h-8 w-8 p-0!"
+                    className="size-8"
                     title={copied ? "Copiado!" : "Copiar link"}
                     onClick={handleCopy}
                 >
-                    { copied ? (
-                        <CheckIcon size={16} />
-                    ) : (
-                        <CopyIcon size={16} />
-                    )}
+                    <CopyIcon className="size-4 shrink-0" />
                 </Button>
 
                 <Button
                     variant="secondary"
-                    className="h-8 w-8 p-0!"
+                    className="h-8 w-8"
                     title="Excluir link"
                     disabled={isDeleting}
                     onClick={() => handleDelete(link.id)}
                 >
-                    <TrashIcon size={16} />
+                    <TrashIcon className="size-4 shrink-0" />
                 </Button>
             </div>
         </div>
